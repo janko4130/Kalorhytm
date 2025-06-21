7182710a501e9f11cdde48404bb10e3fa663d7ae
Jan <jan.kol4130@gmail.com>
2025-06-21 14:53:15 +0200

    adding tailwind.css package

--- Kalorhytm/Kalorhytm.Client/obj/Debug/net8.0/Kalorhytm.Client.AssemblyInfo.cs
+++ Kalorhytm/Kalorhytm.Client/obj/Debug/net8.0/Kalorhytm.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kalorhytm.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c4007597b6f11ef8dbf241c7007dc660720b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kalorhytm.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kalorhytm.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Kalorhytm/Kalorhytm/obj/Debug/net8.0/Kalorhytm.AssemblyInfo.cs
+++ Kalorhytm/Kalorhytm/obj/Debug/net8.0/Kalorhytm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kalorhytm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c4007597b6f11ef8dbf241c7007dc660720b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kalorhytm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kalorhytm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
